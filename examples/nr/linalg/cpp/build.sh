235c2355dca4b0e96be8c3fb271da0cb9737af7b
Joe Hellmers <jlhellmers@sdsu.edu>
2023-10-12 12:36:50 -0700

    Fix build.sh

--- examples/nr/linalg/cpp/build.sh
+++ examples/nr/linalg/cpp/build.sh
@@ -3,7 +3,6 @@
 g++  -Wall -c ludcmp.cpp lubksb.cpp svdcmp.cpp pythag.cpp svbksb.cpp
 g++  -Wall -o xludcmp ludcmp.o xludcmp.cpp
 g++  -Wall -o xlubksb ludcmp.o lubksb.o xlubksb.cpp
-exit
 g++  -Wall -o xsvdcmp svdcmp.o pythag.o xsvdcmp.cpp
 g++  -Wall -o xsvbksb svbksb.o svdcmp.o pythag.o xsvbksb.cpp
 
