9fa4d7e381d6d05e3980520488f3686066dd5e54
Joe Hellmers <jlhellmers@sdsu.edu>
2023-10-12 03:28:19 -0700

    More NR linalg updates

--- examples/nr/linalg/cpp/build.sh
+++ examples/nr/linalg/cpp/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #./clean.sh
-g++ -Wall -c svdcmp.cpp pythag.cpp
-g++ -Wall -o xsvdcmp svdcmp.o pythag.o xsvdcmp.cpp
-#gfortran -Wall -o xsvbksb xsvbksb.f90 nr.o nrutil.o nrtype.o pythag.o svdcmp.o svbksb.o
+g++  -Wall -c ludcmp.cpp lubksb.cpp svdcmp.cpp pythag.cpp svbksb.cpp
+g++  -Wall -o xludcmp ludcmp.o xludcmp.cpp
+g++  -Wall -o xlubksb ludcmp.o lubksb.o xlubksb.cpp
+exit
+g++  -Wall -o xsvdcmp svdcmp.o pythag.o xsvdcmp.cpp
+g++  -Wall -o xsvbksb svbksb.o svdcmp.o pythag.o xsvbksb.cpp
 
