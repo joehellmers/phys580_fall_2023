4265c199ac643a402c8302a772f905654fd05440
Joe Hellmers <jlhellmers@sdsu.edu>
2023-10-25 13:51:15 -0700

    Add update build.sh for Fortran

--- examples/nr/linalg/fortran/build.sh
+++ examples/nr/linalg/fortran/build.sh
@@ -8,3 +8,22 @@ gfortran -Wall -o xsvbksb xsvbksb.f90 nr.o nrutil.o nrtype.o pythag.o svdcmp.o s
 
 g++  -Wall -c ludcmp.cpp lubksb.cpp svdcmp.cpp pythag.cpp svbksb.cpp
 g++  -Wall -o xludcmp ludcmp.o xludcmp.cpp
+
+
+srun --partition=debug  --pty --account=TG-PHY230001 --nodes=1 --ntasks-per-node=4 \
+    --mem=8G -t 00:30:00 --wait=0 --export=ALL /bin/bash
+
+srun --partition=gpu-debug --pty --account=TG-PHY230001 --ntasks-per-node=10 \
+    --nodes=1 --mem=96G --gpus=1 -t 00:30:00 --wait=0 --export=ALL /bin/bash
+
+call ludcmp(KxK,indx,d,K)
+
+Currently Loaded Modules:
+  1) shared   2) slurm/expanse/21.08.8   3) sdsc/1.0   4) DefaultModules   5) gpu/0.15.4 (g)   6) cuda/11.0.2   7) nvhpc/20.9
+
+  Where: 
+
+module load  gpu/0.17.3b
+
+
+ ./configure --with-cuda=$CUDA_HOME --with-cuda-cc=70 --with-cuda-runtime=11.0--prefix=/home/jhellmer/apps/quantum-espresso/7.2
